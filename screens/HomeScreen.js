843860b5e670c65676b279f24721881e42120e63
Lizzy Harvey <lizzy220188@gmail.com>
2022-10-11 09:07:07 -0400

    Fixed Camera

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,14 @@
-import { Camera, CameraType } from 'expo-camera';
 import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
 import * as MedialLibrary from 'expo-media-library';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import {
-  Image,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
 
-import CameraButton from '../components/CameraButton';
-import SmileModal from '../components/SmileModal';
-import ImageModal from '../components/ImageModal'
+import ImageModal from '../components/ImageModal';
+import SmileModal from '../components/HappModal';
+import HappyModal from '../components/HappModal';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
@@ -46,7 +39,7 @@ export default function HomeScreen() {
     if (image) {
       try {
         await MedialLibrary.createAssetAsync(image);
-        alert('Picture Saved!!');
+        alert('Success');
         setImage(null);
       } catch (e) {
         console.log(e);
@@ -55,135 +48,185 @@ export default function HomeScreen() {
   };
 
   if (hasCameraPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>Permission Denied</Text>;
   }
+
   return (
-    <View style={styles.container}>
-      {/* {!Image ? ( */}
-      <Camera
-        style={styles.camera}
-        type={type}
-        flashMode={flash}
-        ref={cameraRef}
-      >
-        <View style={styles.topIcons}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Image
+    <View style={styles.cameraContainer}>
+      {!image ? (
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={flash}
+          ref={cameraRef}
+        >
+          <View style={styles.Header}>
+            <TouchableOpacity
               style={styles.profilePic}
-              source={{
-                uri: 'https://helios-i.mashable.com/imagery/articles/04tV2ASy4BKViKIw4pPBuMi/hero-image.fill.size_1200x1200.v1623368074.jpg',
-              }}
-            />
-            <TouchableOpacity style={{ left: '20%' }}>
-              <Ionicons name='search' size={30} color='white' />
-            </TouchableOpacity>
-            <TouchableOpacity style={{ left: '314%' }}>
-              <Ionicons name='person-add' size={30} color='white' />
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity>
-            <View style={{ marginTop: '35%',}}>
-              <Ionicons
-                name='git-compare'
-                size={29}
-                color='white'
-                onPress={() => {
-                  setType(
-                    type === CameraType.back
-                      ? CameraType.front
-                      : CameraType.back
-                  );
-                }}
+              onPress={() => navigation.navigate('Chat')}
+            >
+              <Image
+                source={{ uri: 'https://picsum.photos/200/300' }}
+                style={{ width: '90%', height: '90%', borderRadius: '100%' }}
               />
-            </View>
-            <Ionicons
-              size={29}
-              color={
-                flash === Camera.Constants.FlashMode.off ? 'gray' : 'white'
-              }
-              name={
-                flash === Camera.Constants.FlashMode.off ? 'flash-off' : 'flash'
-              }
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.search}>
+              <Ionicons name='search-outline' size={35} color={'white'} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.addFriend}>
+              <Ionicons name='person-add' size={35} color={'white'} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.ellipsis}
               onPress={() => {
-                setFlash(
-                  flash === Camera.Constants.FlashMode.off
-                    ? Camera.Constants.FlashMode.on
-                    : Camera.Constants.FlashMode.off
+                setType(
+                  type === CameraType.back ? CameraType.front : CameraType.back
                 );
               }}
-            />
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row-reverse',
-            justifyContent: 'center',
-            marginTop: '147%',
-            alignItems: 'center',
-          }}
-        >
-          <TouchableOpacity style={styles.happy}>
-            <SmileModal />
-          </TouchableOpacity>
-          <CameraButton onPress={takePicture} />
-          <TouchableOpacity style={styles.images}>
-            <ImageModal />
-          </TouchableOpacity>
-        </View>
-      </Camera>
-      {/* ) : (
+            >
+              <Ionicons
+                name={'git-compare-outline'}
+                size={35}
+                color={'white'}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.flash}> 
+                <Ionicons
+                  name={
+                    flash === Camera.Constants.FlashMode.off
+                      ? 'flash-off-outline'
+                      : 'flash-outline'
+                  }
+                  size={35}
+                  color={
+                    flash === Camera.Constants.FlashMode.off ? 'gray' : 'white'
+                  }
+                  onPress={() => {
+                    setFlash(
+                      flash === Camera.Constants.FlashMode.off
+                        ? Camera.Constants.FlashMode.on
+                        : Camera.Constants.FlashMode.off
+                        );
+                      }}
+                      />
+            </TouchableOpacity>
+            </View>
+        </Camera>
+      ) : (
         <Image source={{ uri: image }} style={styles.camera} />
       )}
-
       <View>
-        {!image ? (
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              paddingHorizontal: '12%',
-            }}
-          >
-            <IconButton icon='sync' title={'Re-take'} />
-            <IconButton
-              icon='checkmark-outline'
-              title={'Save'}
-              onPress={savePicture}
-            />
+        {image ? (
+          <View style={{ flex: 1 }}>
+            <View style={{ bottom: '10%', left: '10%', position: 'absolute' }}>
+              <TouchableOpacity onPress={() => setImage(null)}>
+                <Ionicons name='reload-outline' size={50} color='white' />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity onPress={savePicture}>
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom:'10%',
+                    left: '80%',
+                  }}
+                >
+                  <Ionicons name='checkmark-circle-outline' size={50} color='white' />
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
         ) : (
-          <CameraButton onPress={takePicture} />
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <TouchableOpacity style={styles.picture} onPress={takePicture} />
+            <TouchableOpacity style={styles.pictureIcon}>
+              <ImageModal />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.happy}>
+              <ImageModal />
+            </TouchableOpacity>
+          </View>
         )}
-      </View> */}
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
-    justifyContent: 'center',
+  cameraContainer: {
     flex: 1,
   },
   camera: {
     flex: 1,
   },
-  topIcons: {
-    flexDirection: 'row',
-    top: '12%',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    paddingLeft: '5%',
-    paddingRight: '5%',
+  picture: {
+    width: 80,
+    height: 80,
+    borderRadius: '100%',
+    borderColor: 'white',
+    borderWidth: '3%',
+    position: 'absolute',
+    bottom: 30,
+    flex: 1,
+    alignSelf: 'center',
+  },
+  pictureIcon: {
+    position: 'absolute',
+    bottom: 50,
+    left: 110,
+    flex: 1,
   },
   happy: {
+    position: 'absolute',
+    bottom: 50,
+    right: 110,
+    flex: 1,
   },
-  images: {
-    paddingRight: '3%',
+  toggleCamera: {
+    position: 'absolute',
+    right: 20,
+    top: 50,
+  },
+  Header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: '18%',
+  },
+  search: {
+    position: 'absolute',
+    left: '20%',
   },
   profilePic: {
-    width: 50,
-    height: 50,
+    width: 60,
+    height: 60,
     borderRadius: '100%',
+    position: 'absolute',
+    left: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addFriend: {
+    position: 'absolute',
+    right: '15%',
+  },
+  ellipsis: {
+    position: 'absolute',
+    right: '4%',
+    alignItems: 'center',
+  },
+  flash: {
+    position: 'absolute',
+    right: '4%',
+    top: 25,
   },
 });
