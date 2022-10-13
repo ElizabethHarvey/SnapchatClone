9bd1e22fb981c4ab9f7235a97d99299ffecd58b1
Lizzy Harvey <lizzy220188@gmail.com>
2022-10-13 08:46:02 -0400

    Passed all values to StoriesScreen

--- screens/StoriesScreen.js
+++ screens/StoriesScreen.js
@@ -1,20 +1,57 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
- 
+
 import StoriesHeader from '../components/StoriesHeader';
+import FriendsList from '../components/FriendsList';
+import SubscriptionList from '../components/SubscriptionsList';
+import ForYouList from '../components/ForYouList';
 
 function StoriesScreen(props) {
   return (
     <SafeAreaView style={styles.container}>
       <StoriesHeader />
+      <Text style={styles.text}>Friends</Text>
+      <FriendsList />
+      <TouchableOpacity style={{ flexDirection: 'row' }}>
+        <Text style={styles.subText}>Subscriptions</Text>
+        <Text style={styles.arrowHead}>{' >'}</Text>
+      </TouchableOpacity>
+      <SubscriptionList />
+        <Text style={styles.ForYou}>ForYou</Text>
+      <ForYouList />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
+  subText: {
+    left: '20%',
+    marginTop: '5%',
+    marginBottom: '3%',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  arrowHead: {
+    marginTop: '5%',
+    marginBottom: '%',
+    fontWeight: 'bold',
+    fontSize: 20,
+    left: '20%',
+    color: 'lightgrey',
+  },
+  ForYou: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    bottom: '9%',
+    left: '5%',
+  },
+  text: {
+    marginTop: '5%',
+    marginBottom: '3%',
+    fontWeight: 'bold',
+    fontSize: 20,
+    left: '3%',
   },
 });
 
