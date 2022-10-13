2b388ca69466a94b98a9bbdbdd96664927da2eb1
Lizzy Harvey <lizzy220188@gmail.com>
2022-10-13 16:41:10 -0400

    The Dummy Data and the list and row to the DiscoverScreen

---
+++ components/DiscoverSubList.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { StyleSheet, View, Text, FlatList, Platform } from 'react-native';
+import DiscoverSubRow from './DiscoverSubRow';
+import FriendsRow from './FriendsRow';
+import MessageRow from './MessageRow';
+import SubscriptionsRow from './SubscriptionsRow';
+
+const DiscoverSubList = (props) => {
+  const DUMMY_DATA = [
+    {
+      id: 1,
+      title: 'Daddy',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      time: 'today',
+    },
+    {
+      id: 2,
+      title: 'Mommy',
+      image: 'https://picsum.photos/200/300',
+      time: 'today',
+    },
+    {
+      id: 3,
+      title: 'Zeke H.',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      time: 'today',
+    },
+    {
+      id: 4,
+      title: 'Ethan H.',
+      image: 'https://picsum.photos/200/300',
+      time: 'today',
+    },
+    {
+      id: 5,
+      title: 'Elijah Harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed'
+    },
+    {
+      id: 6,
+      title: 'harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      time: 'today',
+    },
+    {
+      id: 7,
+      name: 'Zeke',
+      image: 'https://picsum.photos/200/300',
+    },
+    {
+      id: 8,
+      title: 'Lizzy Harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+    },
+    {
+      id: 9,
+      title: 'Ethan Harvey',
+      image: 'https://picsum.photos/200/300',
+      time: 'today',
+    },
+    {
+      id: 10,
+      title: 'Elijah Harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+    },
+  ];
+
+  return (
+    <View>
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        data={DUMMY_DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <DiscoverSubRow image={item.image} title={item.title} />
+        )}
+      />
+    </View>
+  );
+};
+
+export default DiscoverSubList;
---
+++ components/DiscoverSubRow.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const DiscoverSubRow = ({ image, title, }) => {
+  return (
+    <TouchableOpacity style={styles.row}>
+      <View>
+        <Image style={styles.image} source={{ uri: image }} />
+      </View>
+      <Text style={styles.name}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    alignItems: 'center',
+    padding: '0.2%',
+  },
+  image: {
+    width: 130,
+    height: 200,
+    borderRadius: '10%',
+    left: '10%',
+  },
+  name: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
+    right: '13%',
+    bottom: '10%',
+  },
+});
+
+export default DiscoverSubRow;
