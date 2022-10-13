9a658a29263f165f4aa808c574086e201eb299c1
Lizzy Harvey <lizzy220188@gmail.com>
2022-10-13 16:43:46 -0400

    Added and finished the UI for the DS

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -1,12 +1,95 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const DiscoverScreen = () => {
+import DiscoverHeader from '../components/DiscoverHeader';
+import Weather from '../components/Weather';
+import DiscoverSubList from '../components/DiscoverSubList';
+import TrendingNowList from '../components/TrendingNowList';
+import PopularOnSCList from '../components/PopularOnSCList';
+
+function DiscoverScreen(props) {
   return (
-    <View>
-      <Text>DiscoverScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <DiscoverHeader />
+        <Text style={styles.HappeningNow}>Happening Now</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            bottom: '2%',
+          }}
+        >
+          <Text style={styles.Date}>Sunday, May 24</Text>
+          <View style={styles.dots}>
+            <Ionicons name='notifications' size={27} color='#E7E6E6' />
+          </View>
+        </View>
+        <Weather />
+        <Text style={styles.text}>Discover Subscriptions</Text>
+        <DiscoverSubList />
+        <Text style={styles.text2}>Trending Now</Text>
+        <TrendingNowList />
+        <Text style={styles.text3}>Popular on Snapchat</Text>
+        <PopularOnSCList />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
-export default DiscoverScreen
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+  },
+  HappeningNow: {
+    backgroundColor: 'black',
+    fontSize: '21%',
+    fontWeight: 'bold',
+    color: 'white',
+    left: '2%',
+    marginTop: '2%',
+  },
+  Date: {
+    backgroundColor: 'black',
+    fontSize: '15%',
+    fontWeight: 'bold',
+    color: 'gray',
+    left: '15%',
+  },
+  dots: {
+    borderRadius: '100%',
+    width: 40,
+    height: 40,
+    backgroundColor: '#767676',
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: '2%',
+    right: '50%',
+  },
+  text: {
+    marginTop: '5%',
+    marginBottom: '3%',
+    fontWeight: 'bold',
+    fontSize: 20,
+    left: '3%',
+    color: 'white',
+  },
+  text2: {
+    marginBottom: '3%',
+    fontWeight: 'bold',
+    fontSize: 20,
+    left: '3%',
+    color: 'white',
+  },
+  text3: {
+    marginBottom: '3%',
+    fontWeight: 'bold',
+    fontSize: 20,
+    left: '3%',
+    color: 'white',
+  },
+});
+
+export default DiscoverScreen;
