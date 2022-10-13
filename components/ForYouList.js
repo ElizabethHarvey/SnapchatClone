4930b21151095933b75533c41e825835acc83b79
Lizzy Harvey <lizzy220188@gmail.com>
2022-10-13 12:11:57 -0400

    Fixed up timing

--- components/ForYouList.js
+++ components/ForYouList.js
@@ -12,45 +12,69 @@ const ForYouList = (props) => {
       title: 'Daddy',
       image: 'https://picsum.photos/200/300',
       lock: 'lock-closed',
-      time: 'today',
-      star: '✪'
+      time: '19h ago',
     },
     {
       id: 2,
       title: 'Mommy',
       image: 'https://picsum.photos/200/300',
-      time: 'today',
+      time: '1d ago',
     },
     {
       id: 3,
       title: 'Zeke H.',
       image: 'https://picsum.photos/200/300',
       lock: 'lock-closed',
-      time: 'today',
-      star: '✪'
+      time: '4h ago',
     },
     {
       id: 4,
       title: 'Ethan H.',
       image: 'https://picsum.photos/200/300',
+      time: '12h ago',
+    },
+    {
+      id: 5,
+      title: 'Zeke H.',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      time: '2d ago',
+    },
+    {
+      id: 6,
+      title: 'Ethan H.',
+      image: 'https://picsum.photos/200/300',
       time: 'today',
-      star: '✪'
     },
   ];
+
+  const ListFooter = () => {
+    return <View style={styles.headerFooterStyle}></View>;
+  };
+
 
   return (
     <View>
       <FlatList
-        showsHorizontalScrollIndicator={false}
-        horizontal={true}
+        ListFooterComponent={ListFooter}
+        vertical={true} 
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
         data={DUMMY_DATA}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <ForYouRow image={item.image} title={item.title} time={item.time} star={item.star} />
+          <ForYouRow image={item.image} title={item.title} time={item.time}/>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerFooterStyle: {
+    width: '100%',
+    height: 110,
+  },
+});
 
 export default ForYouList;
