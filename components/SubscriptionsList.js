99ef841f90343b5a2c2f24b94b55c75c746ec134
Lizzy Harvey <lizzy220188@gmail.com>
2022-10-13 08:44:36 -0400

    Added the subscription list

---
+++ components/SubscriptionsList.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { StyleSheet, View, Text, FlatList, Platform } from 'react-native';
+import FriendsRow from './FriendsRow';
+import MessageRow from './MessageRow';
+import SubscriptionsRow from './SubscriptionsRow';
+
+const SubscriptionList = (props) => {
+  const DUMMY_DATA = [
+    {
+      id: 1,
+      title: 'Daddy',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      time: 'today',
+      star: '✪'
+    },
+    {
+      id: 2,
+      title: 'Mommy',
+      image: 'https://picsum.photos/200/300',
+      time: 'today',
+    },
+    {
+      id: 3,
+      title: 'Zeke H.',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      time: 'today',
+      star: '✪'
+    },
+    {
+      id: 4,
+      title: 'Ethan H.',
+      image: 'https://picsum.photos/200/300',
+      time: 'today',
+      star: '✪'
+    },
+    {
+      id: 5,
+      title: 'Elijah Harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed'
+    },
+    {
+      id: 6,
+      title: 'harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      time: 'today',
+      star: '✪'
+    },
+    {
+      id: 7,
+      name: 'Zeke',
+      image: 'https://picsum.photos/200/300',
+      star: '✪',
+    },
+    {
+      id: 8,
+      title: 'Lizzy Harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      star: '✪'
+    },
+    {
+      id: 9,
+      title: 'Ethan Harvey',
+      image: 'https://picsum.photos/200/300',
+      time: 'today',
+    },
+    {
+      id: 10,
+      title: 'Elijah Harvey',
+      image: 'https://picsum.photos/200/300',
+      lock: 'lock-closed',
+      star: '✪'
+    },
+  ];
+
+  return (
+    <View>
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        data={DUMMY_DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SubscriptionsRow image={item.image} title={item.title} time={item.time} star={item.star} />
+        )}
+      />
+    </View>
+  );
+};
+
+export default SubscriptionList;
---
+++ components/SubscriptionsRow.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const SubscriptionRow = ({ image, title, time, star }) => {
+  return (
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.profile}>
+        <Image style={styles.image} source={{ uri: image }} />
+      </View>
+      <Text style={styles.name}>{title}</Text>
+      <View style={{ bottom: '10%', alignItems: 'center'}}>
+        <Text style={styles.time}>{time}</Text>
+        <Text style={styles.star}>{star}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    alignItems: 'center',
+    marginBottom: '90%',
+    padding: '0.5%',
+  },
+  image: {
+    width: 130,
+    height: 200,
+    borderRadius: '10%',
+    left: '10%',
+  },
+  name: {
+    left: '15%',
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: '70%',
+  },
+  time: {
+    fontSize: 15,
+    color: 'gray',
+    position: 'absolute',
+  },
+  star: {
+    color: 'yellow',
+    fontSize: '20%',
+    right: '27%',
+  },
+});
+
+export default SubscriptionRow;
