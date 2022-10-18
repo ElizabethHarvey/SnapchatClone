88487529937129f07b86b9f8807d9afa68ccb569
Lizzy Harvey <lizzy220188@gmail.com>
2022-10-18 12:48:10 -0400

    Finished up birthdayScreen and added react native date picker

--- screens/BirthdayScreen.js
+++ screens/BirthdayScreen.js
@@ -1,18 +1,144 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
-function BirthdayScreen(props) {
+function BirthdayScreen({ navigation }) {
+  const [date, setDate] = useState(new Date('10/18/2022'));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [showIos, setShowIos] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const onChangeIos = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShowIos(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    if (Platform.O == 'android') {
+      setShow(true);
+    }
+    setMode(currentMode);
+  };
+
+  const showModeIOS = (currentMode) => {
+    if (Platform.OS === 'ios') {
+      setShow(true);
+      // for iOS, add a button that closes the picker
+    }
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showDatepickerIOS = () => {
+    showModeIOS('date');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello</Text>
+      <Text style={styles.title}>When's your birthday?</Text>
+      <Text style={styles.birthday}>BIRTHDAY</Text>
+
+      {Platform.OS === 'android' ? (
+        <TouchableOpacity onPress={showDatepicker}>
+          <TextInput
+            placeholderTextColor={'black'}
+            editable={false}
+            style={styles.textInput1}
+            value={date.toLocaleString()}
+          />
+        </TouchableOpacity>
+      ) : null}
+      {Platform.OS === 'ios' ? (
+        <TextInput
+          placeholderTextColor={'black'}
+          editable={false}
+          style={styles.textInput1}
+          value={date.toLocaleString()}
+          onPressIn={showDatepickerIOS}
+          width='70%'
+        />
+      ) : null}
+      {Platform.OS === 'android'
+        ? show && (
+            <DateTimePicker
+              testID='dateTimePicker'
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              onChange={onChange}
+            />
+          )
+        : null}
+      {Platform.OS === 'ios'
+        ? show && (
+            <DateTimePicker
+              testID='dateTimePicker'
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              onChange={onChangeIos}
+              style={styles.iosPicker}
+            />
+          )
+        : null}
+      <TouchableOpacity style={styles.button}>
+        <Button title='Continue' color='white' onPress={() => navigation.navigate('AppNav')}/>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
+  title: {
+    alignSelf: 'center',
+    fontSize: '23%',
+    top: 160,
+  },
+  birthday: {
+    top: 195,
+    left: '13%',
+    color: 'gray',
+    fontSize: '12%',
+  },
+  textInput1: {
+    top: 250,
+    left: '13%',
+    borderBottomWidth: '1%',
+    borderColor: 'lightgray',
+  },
+  iosPicker: {
+    top: '80%',
+    right: '17%',
+  },
+  button: {
+    borderWidth: '1%',
+    borderRadius: '100%',
+    width: '50%',
+    height: '20%',
+    justifyContent: 'center',
+    top: '165%',
+    alignSelf: 'center',
+    backgroundColor: 'lightgray',
+    borderColor: 'lightgray',
+    fontWeight: 'bold',
   },
 });
 
