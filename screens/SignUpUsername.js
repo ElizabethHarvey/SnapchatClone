53bf0b870ea9ca27dcb6648c32f44f56fcc0b949
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-01 14:09:12 -0400

    Added new screen

---
+++ screens/SignUpUsername.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SignUpUsername = () => {
+  return (
+    <View>
+      <Text>SignUpUsername</Text>
+    </View>
+  )
+}
+
+export default SignUpUsername
