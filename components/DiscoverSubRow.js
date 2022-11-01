aeb1d8cdc1639e0226b8dca2994a237649256f32
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-01 14:17:33 -0400

    Refactored Code

--- components/DiscoverSubRow.js
+++ components/DiscoverSubRow.js
@@ -11,7 +11,7 @@ import {
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-const DiscoverSubRow = ({ image, title, }) => {
+const DiscoverSubRow = ({ image, title }) => {
   return (
     <TouchableOpacity style={styles.row}>
       <View>
