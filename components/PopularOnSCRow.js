4a753ace684b41b5c5e6db0f2e878a57996d74b2
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-01 14:18:57 -0400

    Refactored Code

--- components/PopularOnSCRow.js
+++ components/PopularOnSCRow.js
@@ -11,35 +11,35 @@ import {
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-const PopularOnSCRow = ({ image, title, }) => {
-    return (
-      <TouchableOpacity style={styles.row}>
-        <View>
-          <Image style={styles.image} source={{ uri: image }} />
-        </View>
-        <Text style={styles.name}>{title}</Text>
-      </TouchableOpacity>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    row: {
-      alignItems: 'center',
-      padding: '0.2%',
-    },
-    image: {
-      width: 150,
-      height: 250,
-      borderRadius: '10%',
-      left: '10%',
-    },
-    name: {
-      fontSize: 15,
-      color: 'white',
-      fontWeight: 'bold',
-      right: '13%',
-      bottom: '10%',
-    },
-  });
+const PopularOnSCRow = ({ image, title }) => {
+  return (
+    <TouchableOpacity style={styles.row}>
+      <View>
+        <Image style={styles.image} source={{ uri: image }} />
+      </View>
+      <Text style={styles.name}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    alignItems: 'center',
+    padding: '0.2%',
+  },
+  image: {
+    width: 150,
+    height: 250,
+    borderRadius: '10%',
+    left: '10%',
+  },
+  name: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
+    right: '13%',
+    bottom: '10%',
+  },
+});
 
 export default PopularOnSCRow;
