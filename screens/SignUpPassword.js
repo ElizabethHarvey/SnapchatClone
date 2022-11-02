3ef48f7b70e2f33eb942b999e7c5e01802deb652
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-02 12:50:51 -0400

    Finished UI

--- screens/SignUpPassword.js
+++ screens/SignUpPassword.js
@@ -1,12 +1,93 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+import React, { useState } from 'react';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const SignUpPassword = () => {
+const SignUpPassword = ({ navigation }) => {
   return (
-    <View>
-      <Text>SignUpPassword</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <TouchableOpacity style={{ left: '5%' }}>
+          <Ionicons
+            name='chevron-back-outline'
+            size='30%'
+            color='#11ACFA'
+            onPress={() => navigation.navigate('User')}
+          />
+        </TouchableOpacity>
+        <Text style={styles.login}>Set a password</Text>
+        <Text style={styles.text}>Your password should be at least 8</Text>
+        <Text style={styles.text2}>characters.</Text>
+        <Text style={styles.username}>PASSWORD</Text>
+        <TextInput
+          style={styles.textInput1}
+          numberOfLines={1}
+          width='80%'
+          maxLength={15}
+        />
+        <View style={styles.button}>
+          <Button
+            title='Continue'
+            color='white'
+            onPress={() => navigation.navigate('Email')}
+          />
+        </View>
+    </SafeAreaView>
   );
-};
+};``
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  login: {
+    fontSize: '22%',
+    alignSelf: 'center',
+    top: '15%',
+  },
+  username: {
+    top: '20%',
+    left: '10%',
+    letterSpacing: '1%',
+    color: 'gray',
+    fontSize: '12%',
+  },
+  textInput1: {
+    top: '22.5%',
+    left: '10%',
+    borderBottomWidth: '1%',
+    borderColor: 'lightgray',
+  },
+  button: {
+    borderWidth: '1%',
+    borderRadius: '100%',
+    width: '55%',
+    height: '5.5%',
+    top: '60%',
+    alignSelf: 'center',
+    backgroundColor: '#11ACFA',
+    borderColor: '#11ACFA',
+    fontWeight: 'bold',
+    position: 'absolute',
+    justifyContent: 'center',
+  },
+  text: {
+    top: '16%',
+    alignSelf: 'center',
+    color: '#585858'
+  },
+  text2: {
+    top: '16.3%',
+    alignSelf: 'center',
+    color: '#585858'
+  },
+});
 
 export default SignUpPassword;
