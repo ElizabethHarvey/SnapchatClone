713f8972e010add90331b4b612000d41a09beaa3
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-09 14:12:48 -0500

    Added usernames to database

--- screens/SignUpUsername.js
+++ screens/SignUpUsername.js
@@ -8,37 +8,57 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { auth, usersCollection } from '../firebase';
 
 const SignUpUsername = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+
+  const update = {
+    displayUsername: username,
+  };
+
+  const addUsername = () => {
+    auth.currentUser
+    .updateProfile(update)
+    .then(() => {
+      usersCollection
+        .doc(auth.currentUser.uid)
+        .update({
+          Username: username,
+        })
+        .then(() => {
+          console.log('success');
+        });
+    })
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-        <TouchableOpacity style={{ left: '5%', top: '3%' }}>
-          <Ionicons
-            name='chevron-back-outline'
-            size='30%'
-            color='#11ACFA'
-            onPress={() => navigation.navigate('Birthday')}
-          />
-        </TouchableOpacity>
-        <Text style={styles.login}>Pick a username</Text>
-        <Text style={styles.text}>Your username is how friends add you</Text>
-        <Text style={styles.text2}>on Snapchat.</Text>
-        <Text style={styles.username}>USERNAME</Text>
-        <TextInput
-          style={styles.textInput1}
-          numberOfLines={1}
-          width='80%'
-          maxLength={15}
+      <TouchableOpacity style={{ left: '5%', top: '3%' }}>
+        <Ionicons
+          name='chevron-back-outline'
+          size='30%'
+          color='#11ACFA'
+          onPress={() => navigation.navigate('Birthday')}
         />
-        <View style={styles.button}>
-          <Button
-            title='Continue'
-            color='white'
-            onPress={() => navigation.navigate('Email')}
-          />
-        </View>
+      </TouchableOpacity>
+      <Text style={styles.login}>Pick a username</Text>
+      <Text style={styles.text}>Your username is how friends add you</Text>
+      <Text style={styles.text2}>on Snapchat.</Text>
+      <Text style={styles.username}>USERNAME</Text>
+      <TextInput
+        style={styles.textInput1}
+        numberOfLines={1}
+        width='80%'
+        maxLength={50}
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <View style={styles.button}>
+        <Button title='Continue' color='white' onPress={addUsername} />
+      </View>
     </SafeAreaView>
   );
 };
@@ -60,7 +80,7 @@ const styles = StyleSheet.create({
   },
   textInput1: {
     top: '21%',
-    left: '13%', 
+    left: '13%',
     borderBottomWidth: '1%',
     borderColor: 'lightgray',
   },
