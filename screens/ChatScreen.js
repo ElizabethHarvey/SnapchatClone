1b1af439792f16f7389b6de33ada78b928498dbe
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-23 16:08:49 -0500

    Added some messaging functionality

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Image,
   ScrollView,
@@ -14,11 +14,37 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import Header from '../components/ChatHeader';
 import MessageList from '../components/MessageList';
 import SenderMessage from '../components/SenderMessage';
+import { auth, usersCollection } from '../firebase';
+import MessageRow from '../components/MessageRow';
 
 function ChatScreen(props) {
+  // const [messages, setMessages] = useState([]);
+
+  // useEffect(() => {
+  //   const subscriber = usersCollection
+  //     .doc(auth.currentUser.uid)
+  //     .collection('messages')
+  //     .where('room', 'array-contains', route.params.uid)
+  //     .orderby('createdAt', 'desc')
+  //     .onSnapshot((querySnapshot) => {
+  //       const allSentMessages = []
+  //       querySnapshot.forEach((documentSnapshot) => {
+  //         allSentMessages.push({
+  //           ...documentSnapshot.data(),
+  //           key: documentSnapshot.id,
+  //         });
+  //       });
+
+  //       setMessages(allSentMessages);
+  //     });
+  //   return () => subscriber();
+  // }, [usersCollection]);
+  
+
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <MessageList onPress={() => navigation.navigate('Header')} />
     </SafeAreaView>
   );
 }
