fe56daaf6400f89753e6cacdf61803a62fe02228
Lizzy Harvey <lizzy220188@gmail.com>
2022-11-30 18:11:02 -0500

    Added more styling

--- components/SCTextInput.js
+++ components/SCTextInput.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 function SCTextInput(props) {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.textInputBackground}>
-        <TextInput width='55%'/>
-      </TouchableOpacity>
+    <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        <TouchableOpacity style={styles.camera}>
+          <Ionicons name='camera' size={25} color='gray' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.textInputBackground}>
+          <TextInput width='55%' placeholder='' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.mic}>
+          <Ionicons name='mic' size={20} color='#000' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.happy}>
+          <Ionicons name='happy-outline' size={20} color='#000' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.copy}>
+          <Ionicons name='copy-outline' size={20} color='#000' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.rock}>
+          <Ionicons name='rocket-outline' size={20} color='#000' />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
-  },
   textInputBackground: {
-    backgroundColor: '#000',
+    backgroundColor: '#E5E4E2',
     borderRadius: '100%',
     width: '60%',
-    height: '33%',
+    height: '70%',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  camera: {
+    backgroundColor: '#E5E4E2',
+    borderRadius: '100%',
+    width: '10%',
+    height: '70%',
+    alignItems: 'center',
   },
 });
 
